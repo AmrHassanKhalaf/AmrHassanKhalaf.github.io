371f87ac3fb24af6bc7ddfccbac69f72a87c3ed1
AmrHassanKhalaf <amrhassankofficial@gmail.com>
2025-02-26 22:43:29 +0200

    Add files via upload

---
+++ Unit (1).py
@@ -0,0 +1,4 @@
+print("Welcome to the youtube channel name generator:")
+nickname = input("what is your nickname?\n")
+channel_field = input("what is your channel about?\n")
+print("you could name your channel ("+channel_field + " with "+nickname+")")
